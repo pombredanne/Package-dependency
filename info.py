ff72adb38cfe404b7a7c05d656106363f499a84f
Ilia Konnov <iliakonnov@gmail.com>
2016-08-08 18:17:10 +0300

    Added support of many jsons at once

--- info.py
+++ info.py
@@ -6,15 +6,18 @@ from collections import OrderedDict
 
 
 parser = argparse.ArgumentParser(description='Get info about package.')
-parser.add_argument('data', metavar='D', type=str,
+parser.add_argument('data', metavar='D', type=str, nargs='+',
                     help='Json file converted from Packages file')
 
 args = parser.parse_args()
-data = args.data
+dataArgs = args.data
 
 print('Loading data...')
-with open(data, 'r') as f:
-    data = json.load(f)
+loadedData = {}
+for fName in dataArgs:
+    print(fName)
+    with open(fName, 'r') as f:
+        loadedData[fName] = json.load(f)
 
 words = OrderedDict([
     ('package', 'Package name'),
@@ -33,19 +36,23 @@ words = OrderedDict([
 package = raw_input('\npackage> ')
 while package:
     if package == '/all':
-        packages = data.keys()
-        packages = [packages[i:i + 3] for i in xrange(0, len(packages), 3)]
-        packages = map(lambda x: map(str, x), packages)
-        packages = map(', '.join, packages)
-        print('\n'.join(map(str, packages)))
+        for dataName, data in loadedData.iteritems():
+            print(dataName)
+            packages = data.keys()
+            packages = [packages[i:i + 3] for i in xrange(0, len(packages), 3)]
+            packages = map(lambda x: map(str, x), packages)
+            packages = map(', '.join, packages)
+            print('\n\t'.join(map(str, packages)))
     else:
-        if package in data.keys():
-            for key, value in words.iteritems():
-                result = data[package][key]
-                if result:
-                    print('{v}: {r}'.format(v=value, r=result))
-        else:
-            print('Package not found')
+        for dataName, data in loadedData.iteritems():
+            print(dataName)
+            if package in data.keys():
+                for key, value in words.iteritems():
+                    result = data[package][key]
+                    if result:
+                        print('\t{v}: {r}'.format(v=value, r=result))
+            else:
+                print('\tPackage not found')
     try:
         package = raw_input('\npackage> ')
     except (KeyboardInterrupt, EOFError) as e:
--- solve.py
+++ solve.py
@@ -5,34 +5,38 @@ import argparse
 import dependencies
 
 parser = argparse.ArgumentParser(description='Search in repository and solve dependencies')
-parser.add_argument('data', metavar='D', type=str,
+parser.add_argument('data', metavar='D', type=str, nargs='+',
                     help='json file converted from Packages file')
 parser.add_argument('-V', action='store_true', dest='ignoreVersions', default=False,
                     help='ignore versions')
 
 args = parser.parse_args()
-data = args.data
 ignoreVersions = args.ignoreVersions
+dataArgs = args.data
 
 print('Loading data...')
-with open(data, 'r') as f:
-    data = json.load(f)
-
+loadedData = {}
+for fName in dataArgs:
+    print(fName)
+    with open(fName, 'r') as f:
+        loadedData[fName] = json.load(f)
 
 if ignoreVersions:
     print('WARNING: Ignoring version!')
 
 package = raw_input('package> ')
 while package:
-    if package in data.keys():
-        result, error = dependencies.solve(data, package, ignoreVersions)
+    for dataName, data in loadedData.iteritems():
+        print(dataName)
+        if package in data.keys():
+            result, error = dependencies.solve(data, package, ignoreVersions)
 
-        result = ', '.join(map(str, result))
-        error = map(lambda x: map(str, x), error)
-        error = ', '.join(map(str, error))
+            result = ', '.join(map(str, result))
+            error = map(lambda x: map(str, x), error)
+            error = ', '.join(map(str, error))
 
-        print('\nresult: {}'.format(result))
-        print('error: {}'.format(error))
-    else:
-        print('Package not found')
+            print('\tresult: {}'.format(result))
+            print('\terror: {}\n'.format(error))
+        else:
+            print('\tPackage not found')
     package = raw_input('package> ')
